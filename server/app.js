9720bf60babb45b4eeb26edc1c07863535cf314f
Denis <tde261@yandex.ru>
2018-07-16 22:55:18 +0300

    Another one

--- server/app.js
+++ server/app.js
@@ -31,6 +31,15 @@ app.delete('/notes/:id', (req, res) => {
     db.deleteNote(req.params.id).then(data => res.send(data));
 });
 
+//HEROKU
+const server = app.listen(os.Getenv("PORT"), function() {
+    console.log(`Server is up and running on port ${os.Getenv("PORT")}`);
+});
+
+
+//LOCALHOST
+/*
 const server = app.listen(serverPort, function() {
     console.log(`Server is up and running on port ${serverPort}`);
 });
+*/
